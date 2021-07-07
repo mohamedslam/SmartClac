58634ac86335e7f4088162df9c8a3efa868d0d76
mohamedslam <mohamedslam2000@yahoo.com>
2021-07-07 10:05:35 +0500

    Add project files.

---
+++ Class/ClsClaculation.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartClac.Class
+{
+   public class Claculation
+    {
+      static  string _UserFormula = "";
+
+       
+      
+
+        private static string[] _operatorsOfOperation = { "-", "+", "/", "*", "^" };
+        private static Func<double, double, double>[] MyAllowanceOperation = {
+        (a1, a2) => a1 - a2,
+        (a1, a2) => a1 + a2,
+        (a1, a2) => a1 / a2,
+        (a1, a2) => a1 * a2,
+        (a1, a2) => Math.Pow(a1, a2)
+    };
+
+        public static double Claculate(string expression)
+        {
+            List<string> tokens = getTokens(expression);
+            Stack<double> operandStack = new Stack<double>();
+            Stack<string> operatorStack = new Stack<string>();
+            int tokenIndex = 0;
+
+            while (tokenIndex < tokens.Count)
+            {
+                string token = tokens[tokenIndex];
+                if (token == "(")
+                {
+                    string subExpr = GetSeperatedSubExpression(tokens, ref tokenIndex);
+                    operandStack.Push(Claculate(subExpr));
+                    continue;
+                }
+                if (token == ")")
+                {
+                    throw new ArgumentException("Mis-matched parentheses in expression");
+                }
+                //check is that  operator  or not ?
+                if (Array.IndexOf(_operatorsOfOperation, token) >= 0)
+                {
+                    while (operatorStack.Count > 0 && Array.IndexOf(_operatorsOfOperation, token) < Array.IndexOf(_operatorsOfOperation, operatorStack.Peek()))
+                    {
+                        string op = operatorStack.Pop();
+                        double arg2 = operandStack.Pop();
+                        double arg1 = operandStack.Pop();
+                        operandStack.Push(MyAllowanceOperation[Array.IndexOf(_operatorsOfOperation, op)](arg1, arg2));
+                    }
+                    operatorStack.Push(token);
+                }
+                else
+                {
+                    operandStack.Push(double.Parse(token));
+                }
+                tokenIndex += 1;
+            }
+
+            while (operatorStack.Count > 0)
+            {
+                string op = operatorStack.Pop();
+                double arg2 = operandStack.Pop();
+                double arg1 = operandStack.Pop();
+                operandStack.Push(MyAllowanceOperation[Array.IndexOf(_operatorsOfOperation, op)](arg1, arg2));
+            }
+            return operandStack.Pop();
+        }
+
+        private static string GetSeperatedSubExpression(List<string> tokens, ref int index)
+        {
+            StringBuilder subFormula = new StringBuilder();
+            int parenlevels = 1;
+            index += 1;
+            while (index < tokens.Count && parenlevels > 0)
+            {
+                string token = tokens[index];
+                if (tokens[index] == "(")
+                {
+                    parenlevels += 1;
+                }
+
+                if (tokens[index] == ")")
+                {
+                    parenlevels -= 1;
+                }
+
+                if (parenlevels > 0)
+                {
+                    subFormula.Append(token);
+                }
+
+                index += 1;
+            }
+
+            if ((parenlevels > 0))
+            {
+                throw new ArgumentException("Brackets do not match");
+            }
+            return subFormula.ToString();
+        }
+
+        private static List<string> getTokens(string expression)
+        {
+            string operators = "()^*/+-";
+            List<string> tokens = new List<string>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in expression.Replace(" ", string.Empty))
+            {
+                if (operators.IndexOf(c) >= 0)
+                {
+                    if ((sb.Length > 0))
+                    {
+                        tokens.Add(sb.ToString());
+                        sb.Length = 0;
+                    }
+                    tokens.Add(c.ToString());
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            if ((sb.Length > 0))
+            {
+                tokens.Add(sb.ToString());
+            }
+            return tokens;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using SmartClac.Class;
+using System;
+
+namespace SmartClac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            bool _Resume = true;
+            do
+            {
+
+
+                Console.WriteLine("Insert Formula To calculate it");
+                var _UserFormula = Console.ReadLine();
+                var Result = "";
+
+                
+                Result =Claculation.Claculate(_UserFormula).ToString();
+                 
+
+                Console.WriteLine("The Result Of  Formula : "+_UserFormula+" =  "+Result);
+               
+
+                Console.WriteLine("If You want Claculate Another Formula press y/Y");
+                string val_Resume = Console.ReadLine();
+
+
+                if (val_Resume == "Y" || val_Resume == "y")
+                    _Resume = true;
+                else
+                    _Resume = false;
+
+            }
+            while (_Resume);
+
+
+        }
+    }
+}
